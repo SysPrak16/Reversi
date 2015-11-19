9eac40d39b6e81ddeab9ec70c414f2c041d6a1b7
Philipp Altmann <altmannp@cip.ifi.lmu.de>
2015-11-19 18:49:57 +0100

    added appParamHandler for checking GameID

---
+++ lib/appParamHandler.c
@@ -0,0 +1,31 @@
+//
+// Created by Philipp on 19.11.15.
+//
+
+#define errorString "\0"
+/* @param: int argc, char *argv[]: Kommandozeilenparrameter aus main()
+ * @ret: -1 bei Fehler (non valid Input numer) wrongParamError
+ * @ret:
+ *
+ * */
+char* checkParam(int argc, char *argv[]){
+    if(argc < 1 || argc > 2) {
+        return errorString; //wrongParamError
+    }
+    else{
+        char *GameID;
+        GameID = malloc(11 * sizeof(char));
+        int count = 0;
+        while(count<11){
+            if(argv[1][count]=='\0'){
+                return errorString; //wrongParamError
+            }
+            GameID[count]=argv[1][count];
+            count++;
+        }
+        if(argv[1][count]!='\0'){
+            return errorString; //wrongParamError
+        }
+        return GameID;
+    }
+}
--- lib/appParamHandler.h
+++ lib/appParamHandler.h
@@ -3,10 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 /*
  * This is a library which exclusively is responsible for console command parameters
  * which can be called from within the project.
 */
+
+char* checkParam(int argc, char *argv[]);
 
 #endif // PERFORMCONNECTION_H
