1dd3f732772f2b5a3b8b110ed378595a021665f0
Philipp Altmann <altmannp@cip.ifi.lmu.de>
2015-11-23 15:29:53 +0100

    Change Save

--- main.c
+++ main.c
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 //
 // Created by Philipp on 13.11.15.
 //
 
 #include <stdio.h>
 #include "main.h"
-
-int main (void){
-    printf("Hello World!");
-    return 0;
-}
-=======
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
