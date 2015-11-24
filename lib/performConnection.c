eaee1d526b22605c09adee243d69c77421550322
KadVenku <venkukad@gmail.com>
2015-11-24 08:35:24 +0100

    Up 11-24-15-001  Changes to be committed: 	modified:   lib/performConnection.c
     Untracked files:
    	.idea/

--- lib/performConnection.c
+++ lib/performConnection.c
@@ -14,13 +14,13 @@ int performConnection(int socket_fd)
 	//n is the return value for the read() and write() calls; i.e. it contains the number of characters read or written.
 	
 	// ToDo: Schleife fuer aufeinanderfolgendes Empfangen und Senden
-	//for(i = 0;i<5;i++) {
+	for(i = 0;i<4;i++) {
 		bzero(buffer, 256);    //buffer leeren
 		bzero(line, 256);    //buffer leeren
 		//n = read(socket_fd, buffer, 100);
 		int cancel = 1;// Wird in while Schleife auf 0 gesetzt wenn '\n' empfangen wurde. -> Abbruchbedingung
 
-		while ((n = recv(socket_fd, buffer, 256, 0)) > 0 && cancel) {
+		while ((n = recv(socket_fd, buffer, 256, 0)) > 0){ //&& cancel==1) {
 			// hier noch Fehler, da nicht bist FileEnde gewartet wird.
 			// ToDo: Buffer bis letztes Zeichen lesen
 			if (n < 0) {
@@ -34,13 +34,13 @@ int performConnection(int socket_fd)
 			bzero(buffer, 256);
 		}
 
-		printf("Antwort: %s\n", line);
-        printf("%s\n",buffer);
+		printf("LINE: %s\n", line);
+        printf("BUFFER: %s\n",buffer);
 		// ToDo: Fallabfrage mit Anwort
         //bzero(buffer, 256);
         buffer[0]='a';
         send(socket_fd, buffer, strlen(buffer),0);
         //printf("Was bist du? %s\n", buffer);
-	//}
+	}
 	return 0;
 }
