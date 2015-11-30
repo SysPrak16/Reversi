93d82a7556522e05efff3638b3e7503c9c07eb88
KadVenku <venkukad@gmail.com>
2015-11-30 12:43:24 +0100

    New server listener

--- lib/performConnection.c
+++ lib/performConnection.c
@@ -12,84 +12,83 @@
 int performConnection(int socket_fd)
 {
     char buffer[BUF_SIZE];
-    char line[BUF_SIZE];
-    int n,i,cancel;
-    bzero(buffer, BUF_SIZE);
-    bzero(line, BUF_SIZE);
+    int n,i;
     //TODO: Actual question and answer game!
-    /*
-     * for now there is only one "right" answer included.
-     * the gameID breaks the server if not correct, so we probably should handle that in the client.
-     * have tested this "feature" extensively, that server is awfully annoying
-     * reminds me: TODO: Ask for debugging server setting with prolonged timeout!!!
-     */
-    //for(i=0;i<2;i++){
-    for(i=0;i<2;i++){
-    //for (;;){
-        cancel=1;
-        bzero(buffer,BUF_SIZE);
-        bzero(line, BUF_SIZE);
-        //TODO: For some reason the second answer is not received completely every time.
-        while(cancel>0 && (n = recv(socket_fd, buffer, 256, 0)) > 0){
-            //printf("Entered While%s\n");
-            if (n<0){
-                perror("ERROR reading from socket");
-                return -1;
-            }
-            if (buffer[n - 1] == '\n') {
-                cancel = 0;
-            }
-            strcat(line, buffer);
-            //printf("BUFFER: %s\n",buffer);
-        }
+    i=0;
+    while((n = recv(socket_fd, buffer, sizeof(buffer), 0)) > 0)
+    {
+        printf("Server: %.*s", n, buffer);
 
-        printf("SERVER: %s\n", line);
-        /*
-         * CLeaning out buffer, just to be sure.
-         * for some reason cleaning out line before buffer works better than the other way around
-        */
-        bzero(line, BUF_SIZE);
-        bzero(buffer, BUF_SIZE);
-        //TODO: replace with actual statements and remove placeholders
-        switch(i) {
+        char sbuf[]="VERSION 1.0\n";
+        char sbuf2[]="ID GXe1oEV1gGs\n";
+        char sbuf3[]="PLAYER 1\n";
+        char sbuf4[]="THINKING\n";
+        char sbuf5[]="PLAY F2\n";
+        char sbuf6[]="OKWAIT\n";
+        switch(i){
             case 0:
-                buffer[0]='V';
-                buffer[1]='E';
-                buffer[2]='R';
-                buffer[3]='S';
-                buffer[4]='I';
-                buffer[5]='O';
-                buffer[6]='N';
-                buffer[7]=' ';
-                buffer[8]='1';
-                buffer[9]='.';
-                buffer[10]='0';
-                buffer[11]='\n';
-                printf("Client: %s\n", buffer);
-                send(socket_fd, buffer, BUF_SIZE,0);
+                if(send(socket_fd , sbuf , strlen(sbuf) , 0) < 0)
+                {
+                    puts("Send failed");
+                    return -1;
+                }else{
+                    printf("Client: %.*s", n, sbuf);
+                }
+                bzero(buffer, BUF_SIZE);
                 break;
             case 1:
-                buffer[0]='I';
-                buffer[1]='D';
-                buffer[2]=' ';
-                buffer[3]='1';
-                buffer[4]='2';
-                buffer[5]='3';
-                buffer[6]='4';
-                buffer[7]='5';
-                buffer[8]='6';
-                buffer[9]='7';
-                buffer[10]='8';
-                buffer[11]='9';
-                buffer[12]='1';
-                buffer[13]='2';
-                buffer[14]='\n';
-                printf("Client: %s\n", buffer);
-                send(socket_fd, buffer, BUF_SIZE,0);
+                if(send(socket_fd , sbuf2 , strlen(sbuf2) , 0) < 0)
+                {
+                    puts("Send failed");
+                    return -1;
+                }else{
+                    printf("Client: %.*s", n, sbuf2);
+                }
+                bzero(buffer, BUF_SIZE);
                 break;
-            default:
-                return -1;
-        }
+            case 2:
+                if(send(socket_fd , sbuf3 , strlen(sbuf3) , 0) < 0)
+                {
+                    puts("Send failed");
+                    return -1;
+                }else{
+                    printf("Client: %.*s", n, sbuf3);
+                }
+                bzero(buffer, BUF_SIZE);
+                break;
+            case 3:
+                if(send(socket_fd , sbuf4 , strlen(sbuf4) , 0) < 0)
+                {
+                    puts("Send failed");
+                    return -1;
+                }else{
+                    printf("Client: %.*s", n, sbuf4);
+                }
+                bzero(buffer, BUF_SIZE);
+                break;
+            case 4:
+                if(send(socket_fd , sbuf5 , strlen(sbuf5) , 0) < 0)
+                {
+                    puts("Send failed");
+                    return -1;
+                }else{
+                    printf("Client: %.*s", n, sbuf5);
+                }
+                bzero(buffer, BUF_SIZE);
+                break;
+            case 5:
+                if(send(socket_fd , sbuf6 , strlen(sbuf6) , 0) < 0)
+                {
+                    puts("Send failed");
+                    return -1;
+                }else{
+                    printf("Client: %.*s", n, sbuf6);
+                }
+                bzero(buffer, BUF_SIZE);
+                break;
+            }
+        i++;
+
     }
     return 0;
 }
