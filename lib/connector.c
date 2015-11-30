f6a85c06cdbc08cc31d8ed2a232bea81247e51a9
KadVenku <venkukad@gmail.com>
2015-11-30 13:41:42 +0100

    receive Update Faelle begonnen

--- lib/connector.c
+++ lib/connector.c
@@ -11,15 +11,16 @@ int receiveMessage(int sfd, char* buffer, int size)
     int n = 0;
 
     while (n < size) {
+        //Liest n Zeichen. Merkt sich  Stelle n und schreibt ab n im Array weiter.
         n += recv(sfd, &buffer[n], size - n, 0);
         if (n == -1) {
             perror("Fehler: recv()");
             break;
         }
-
         buffer[n] = 0;
+        //Abbruch bei vorletzter Stelle \n
         if (n != 0 && buffer[n - 1] == '\n') {
-            fprintf(stdout, "S: %s", buffer);
+            fprintf(stdout, "SERVER: %s", buffer);
             break;
         }
     }
--- lib/performConnection.c
+++ lib/performConnection.c
@@ -1,5 +1,6 @@
 #include "performConnection.h"
 #include "global.h"
+#include "connector.h"
 
 #include <unistd.h>
 #include <stdio.h>
@@ -8,6 +9,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+int strbeg(const char *str1, const char *str2)
+{
+    return strncmp(str1, str2, strlen(str2)) == 0;
+}
 
 int performConnection(int socket_fd)
 {
@@ -15,9 +20,10 @@ int performConnection(int socket_fd)
     int n,i;
     //TODO: Actual question and answer game!
     i=0;
-    while((n = recv(socket_fd, buffer, sizeof(buffer), 0)) > 0)
+    //while((n = recv(socket_fd, buffer, sizeof(buffer), 0)) > 0)
+    while((n = receiveMessage(socket_fd, buffer, sizeof(buffer))) > 0)
     {
-        printf("Server: %.*s", n, buffer);
+        //printf("Server: %.*s", n, buffer);
 
         char sbuf[]="VERSION 1.0\n";
         char sbuf2[]="ID GXe1oEV1gGs\n";
@@ -25,7 +31,36 @@ int performConnection(int socket_fd)
         char sbuf4[]="THINKING\n";
         char sbuf5[]="PLAY F2\n";
         char sbuf6[]="OKWAIT\n";
-        switch(i){
+
+        if (strbeg(buffer, "-")){
+            printf("Server Connection error\n");
+            return -1;
+        } else if (strbeg(buffer, "+ MNM Gameserver")){
+            if(send(socket_fd , sbuf , strlen(sbuf) , 0) < 0)
+            {
+                puts("Send failed");
+                return -1;
+            }else{
+                printf("Client: %.*s", n, sbuf);
+            }
+            //printf("Client sending Client Version\n");
+        } else if (strbeg(buffer, "+ Client version accepted - please send Game-ID to join")){
+            if(send(socket_fd , sbuf2 , strlen(sbuf2) , 0) < 0)
+            {
+                puts("Send failed");
+                return -1;
+            }else{
+                printf("Client: %.*s", n, sbuf2);
+            }
+            //printf("Client sending Game-ID\n");
+        } else if (strbeg(buffer, "+ PLAYING")){
+        } else {
+            printf("Fehlerhafte Nachricht vom Server\n");
+            printf("%s\n", buffer);
+            return -1;
+        }
+
+        /*switch(i){
             case 0:
                 if(send(socket_fd , sbuf , strlen(sbuf) , 0) < 0)
                 {
@@ -86,9 +121,22 @@ int performConnection(int socket_fd)
                 }
                 bzero(buffer, BUF_SIZE);
                 break;
-            }
+            }*/
         i++;
 
     }
     return 0;
+
+    /*if (strbeg(line, "-")){	printf("Server Connection error\n");
+      return -1;
+    	} else if (strbeg(line, "+ MNM Gameserver")){
+            send(socket_fd, "VERSION 1.0", strlen("VERSION 1.0")+1,0);
+		    printf("Client sending Client Version\n");
+	    } else if (strbeg(line, "+ Client version accepted - please send Game-ID tp join")){
+		    printf("Client sending Game-ID\n");
+    	} else if (strbeg(line, "+ PLAYING")){
+    	} else {
+	    	printf("Fehlerhafte Nachricht vom Server\n");
+	    	return -1;
+	    }*/
 }
