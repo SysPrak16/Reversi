427f8ff894c5f8ab7d5a97a6d05b37004fdcc621
nemesisG <madjackal@web.de>
2016-01-25 11:52:30 +0100

    zwei zusätzliche "KI"s hizugefügt

--- lib/thinker.c
+++ lib/thinker.c
@@ -254,6 +254,25 @@ int randomAI (int zuege[], int anzahl){
     return zuege[r];
 }
 
+int sligtlyEnhancedRandomAI (int zuege[], int anzahl, int groesse){
+    int i;
+    for (int i = 0; i<anzahl; i++)      //überprüfe ob folgende Positionen in der Liste sind
+    {
+        if (zuege[i]==0){       //links oben     
+            return 0;}          
+        if (zuege[i]== (groesse-1)){    //rechts oben
+            return (zuege[i]);}
+        if (zuege[i]== (groesse*groesse-1)){    //rechts unten
+            return (zuege[i]);}
+        if (zuege[i]== (groesse*(groesse-1))){  //links unten
+            return (zuege[i]);}
+    }
+    time_t t;       //Ansonsten wähle nen zufälligen Zug
+    srand((unsigned) time(&t));
+    int r = rand() % anzahl;
+    return zuege[r];
+}
+
 char* convertMove(char * spielzug, int position, int groesse){
     //ASCII umrechnung der Position
     // char * spielzug enthält den fertigen String
@@ -271,15 +290,16 @@ char* convertMove(char * spielzug, int position, int groesse){
     return spielzug;
 }
 
-int gueltigeZuege(int *feld, int groesse){
-    int i, n, gain, sizeZuege, zug;
+int gueltigerZug(int *feld, int groesse){
+    int i, n, gain, sizeZuege, zug, currentMaxGain, maxGainZug;
 
     int * zuege = malloc(groesse*groesse * sizeof(int));
     if (NULL == zuege) {
         fprintf(stderr, "malloc failed\n");
         return(-1);
     }
-
+    currentMaxGain = 0;
+    maxGainZug = -1;
     sizeZuege = 0;
     for (i=0; i<(groesse*groesse);i++){
         if (feld[i]==0)
@@ -321,7 +341,12 @@ int gueltigeZuege(int *feld, int groesse){
             {
                 zuege[sizeZuege] = i;
                 sizeZuege++;
-            }}
+            }
+            if (gain > currentMaxGain)
+            {
+                maxGainZug = i;
+            }
+        }
     }
 
     if (sizeZuege == 0)
@@ -329,6 +354,9 @@ int gueltigeZuege(int *feld, int groesse){
         printf("kein gültiger Zug berechenbar");
         return -1;  }
     zug = randomAI(zuege,sizeZuege);
+    // zug = sligtlyEnhancedRandomAI (zuege,sizeZuege,groesse);
+    // Max Gain KI:
+    // zug = maxGainZug;
     free(zuege);
     return zug;
 }
